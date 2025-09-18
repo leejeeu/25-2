5d2898a815d39420dede1694f962a58c81e9bb6f
leejeeu <guabcd05028@gmail.com>
2025-09-19 00:07:43 +0900

    Add files via upload

---
+++ PWM_upgrade2.ino
@@ -0,0 +1,36 @@
+void setup() {
+  pinMode(7,OUTPUT);
+
+}
+float ledPeriod;
+float ledDuty;
+void set_period(int period){
+  ledPeriod = period;
+}
+void set_duty(int duty){
+  ledDuty = (float) duty/100.0;
+}
+float set_on(){
+  return ledPeriod*ledDuty;
+}
+float set_off(){
+  return ledPeriod-set_on();
+}
+void loop() {
+  set_period(10000);
+  for(int i=0; i<=100; i++){
+    set_duty(i);
+    digitalWrite(7,HIGH);
+    delayMicroseconds((int)set_on());
+    digitalWrite(7,LOW);
+    delayMicroseconds((int)set_off());
+  }
+  for(int i=100; i>=0; i--){
+    set_duty(i);
+    digitalWrite(7,HIGH);
+    delayMicroseconds((int)set_on());
+    digitalWrite(7,LOW);
+    delayMicroseconds((int)set_off());
+  }
+
+}
